f050cf144be8944592bbbc420f5da3cbf8d73863
Garrett Gunnell <gunnellg@oregonstate.edu>
2019-11-23 00:22:07 -0800

    Generates basic voxel mesh

--- Assets/VoxelGrid.cs
+++ Assets/VoxelGrid.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class VoxelGrid : MonoBehaviour {
 
+    public int resolution;
+    public GameObject voxelPrefab;
 
+    private bool[] voxels;
+    private float voxelSize;
+
+    private void Awake() {
+        voxelSize = 1f / resolution;
+        voxels = new bool[resolution * resolution];
+        for (int i = 0, y = 0; y < resolution; ++y) {
+            for (int x = 0; x < resolution; ++x, ++i) {
+                CreateVoxel(i, x, y);
+            }
+        }
+    }
+
+    private void CreateVoxel (int i, int x, int y) {
+        GameObject o = Instantiate(voxelPrefab) as GameObject;
+        o.transform.parent = transform;
+        o.transform.localPosition = new Vector3((x + 0.5f) * voxelSize, (y + 0.5f) * voxelSize);
+        o.transform.localScale = Vector3.one * voxelSize;
+    }
 }
