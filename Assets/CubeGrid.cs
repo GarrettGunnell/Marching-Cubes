3774832df031b5814f43a445e332fc185b271762
Garrett Gunnell <gunnellg@oregonstate.edu>
2019-11-28 21:36:22 -0800

    Add collision detection on 3D vertex grid

--- Assets/CubeGrid.cs
+++ Assets/CubeGrid.cs
@@ -9,20 +9,30 @@ public class CubeGrid : MonoBehaviour {
     public float size = 8;
 
     private bool[, ,] vertices;
-    private float distanceMod;
+    private float voxelSize, halfSize;
 
-    void Awake() {
+    private void Awake() {
         vertices = new bool[resolution, resolution, resolution];
-        distanceMod = size / resolution;
+        voxelSize = size / resolution;
+        halfSize = size / 2;
+
         for (int x = 0; x < resolution; ++x) {
             for (int y = 0; y < resolution; ++y) {
                 for (int z = 0; z < resolution; ++z) {
                     GameObject o = Instantiate(cubePrefab) as GameObject;
+                    BoxCollider ob = o.AddComponent<BoxCollider>();
                     o.transform.parent = transform;
-                    o.transform.localPosition = new Vector3(x * distanceMod, y * distanceMod, z * distanceMod);
-                    o.transform.localScale = Vector3.one * distanceMod * 0.05f;
+                    o.transform.localPosition = new Vector3(x * voxelSize, y * voxelSize, z * voxelSize);
+                    o.transform.localScale = Vector3.one * voxelSize * 0.1f;
                 }
             }
         }
+    }
+
+    public void EditVertex(Vector3 point) {
+        int vertexX = (int)(point.x / voxelSize);
+        int vertexY = (int)(point.y / voxelSize);
+        int vertexZ = (int)(point.z / voxelSize);
+        Debug.Log(vertexX + ", " + vertexY + ", " + vertexZ);
     }
 }
